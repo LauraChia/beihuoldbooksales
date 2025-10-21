05455726edd629e80dc0404eb1cd9137cf132124
LauraChia <laurachia061289@gmail.com>
2025-10-21 14:42:47 +0800

    profile.jsp改了電子郵件
    電子郵件呈現從資料庫讀取使用者資料

--- src/main/java/hitstd/group/tool/database/DBConfig.java
+++ src/main/java/hitstd/group/tool/database/DBConfig.java
@@ -7,7 +7,7 @@ public class DBConfig {
 	public String FilePath(){
 	   //YINGBOOK
 		 String DBPath="C:\\Users\\user1\\eclipse-workspace\\book\\src\\main\\webapp\\北護二手書販賣系統 (2).accdb";
-		//String DBPath="C:\\apache-tomcat-10.1.24\\webapps\\ book\\北護二手書販賣系統.accdb";
+		//String DBPath="C:\\apache-tomcat-10.1.24\\webapps\\ book\\北護二手書販賣系統 (2).accdb";
 	   return DBPath;		
 	}
 }
